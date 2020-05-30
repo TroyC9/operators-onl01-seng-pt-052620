9f4fa15523aa3b3c074b11fd318d403702b27bb2
TroyC9 <troy.christy@icloud.com>
2020-05-29 20:31:09 -0400

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
 def unsafe?(speed)
-
+    !(40..60).include?(speed)
+    speed < 40 || speed > 60
 end
+
 
 
 
 def not_safe?(speed)
-	
+    speed_number = speed.class == String ? speed.to_i : speed
+  speed < 40 || speed > 60 ? true : false
 end
 	
-
+def pluarlize(word, number)
+    if number == 1
+        "#{number} #{word}"
+    else
+        "#{number} #{word}s"
+end
+number == 1 ? "#{number}" : "#{number} #{word}s"
+end
 
